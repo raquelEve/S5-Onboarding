f5fee2b441d8f92947bd125c0d99b428c2f3b0c2
raquelEve <calculadoraverde@gmail.com>
2024-04-19 20:52:16 +0200

    S5 ex3 css card design

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Card from './components/card'
+// import logo from "./logo.svg";
 
 function App() {
   const tutorialData = [
@@ -8,17 +9,17 @@ function App() {
       title: 'Dedica moltes hores',
       description: 'Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores.\n Al principi sembla impossible, però notaràs una millora ràpidament.',
       bgColor: "#4da2a9",
-      image: 'img1'
+      image: 'images/time_managment.svg'
     }, {
       title: "Programa projectes propis",
       description: "Més val 10 hores treballant en projectes propis, que 10 hores miranr tutorials.\n La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.",
       bgColor: "#d3d4d9",
-      image: 'img'
+      image: 'images/programming.svg'
     }, {
       title: "Procura descansar",
       description: "Descansar bé i desconectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consolodidaràs el teu aprenentatge.",
       bgColor: "#ffd167",
-      image: 'img'
+      image: 'images/meditation.svg'
     }
   ]
 
@@ -30,10 +31,10 @@ function App() {
       setStep(step + 1);
     }
   }
+  // const imagen =require(`../assets/${imagen}.png`).default;
   return (
     <>
       <Card currentCardData={currentCardData} nextStep={nextStep}></Card>
-      <p>{step}</p>
     </>
   )
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,13 +4,24 @@ import PropTypes from 'prop-types';
 import './card.css'
 
 const Card = ({ currentCardData, nextStep }) => {
+    //to call the function (nextStep) in the parent necessary to pass it through props
+
+    const { title, description, image, bgColor } = currentCardData;
+    // Importar dinámicamente la imagen
+    const databgColor = { backgroundColor: bgColor }
 
     return (
         <div className='card'>
-            <h1>{currentCardData.title}</h1>
-            <p>{currentCardData.description}</p>
-            <div className='button-box'>
-                <button onClick={nextStep}>+</button>
+            {/* the images are in the public folder */}
+            <div className="card-header" style={databgColor}>
+                <img className="card-image" src={image} alt="" />
+            </div>
+            <div className='card-body'>
+                <h1 className='card-title'>{title}</h1>
+                <p>{description}</p>
+                <div className='button-box'>
+                    <button onClick={nextStep}>+</button>
+                </div>
             </div>
         </div>
     );
@@ -21,6 +32,8 @@ Card.propTypes = {
     currentCardData: PropTypes.shape({
         title: PropTypes.string.isRequired,
         description: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        bgColor: PropTypes.string.isRequired,
     }),
     nextStep: PropTypes.func
 };
